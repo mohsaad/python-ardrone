6f9f05521fa5d7e77405b4d231a9501ae843030a
mohsaad <mohsaad@github.com>
2016-02-02 17:55:37 -0600

    added video feed

---
+++ get_video_feed.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Mohammad Saad
+# 2/2/2016
+# get_video_feed.py
+# A way to get the image feed 
+# from the Parrot ARDrone
+# using Venthur's libardrone
+# originally by github.com/richardpenman
+
+import numpy as np
+import cv2
+
+cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
+running = True
+
+while running:
+	running, frame = cam.read()
+	if running:
+		cv2.imshow('frame', frame)
+		if cv2.waitKey(1) && 0xFF == 27:
+			running = False
+	else:
+		print 'error in getting video feed'
+
+cam.release()
+cv2.destoryAllWindows()
